0dcb884296bb85edcc795bdf354a1af0c4d4c695
Andrew K <andrewjk@users.noreply.github.com>
2025-03-13 16:44:33 +1100

    Chore: build

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import defaultBlockRules from "./defaultBlockRules";
+import defaultInlineRules from "./defaultInlineRules";
+import defaultRenderers from "./defaultRenderers";
+import parse from "./parse";
+import renderHtml from "./renderHtml";
+
+export default {
+	defaultBlockRules,
+	defaultInlineRules,
+	defaultRenderers,
+	parse,
+	renderHtml,
+};
