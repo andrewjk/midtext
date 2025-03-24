49a8050ce2cfed4903804da140df8fd71e9ea209
Andrew K <andrewjk@users.noreply.github.com>
2025-03-24 12:11:21 +1100

    Fix: make autolinks greedy like code and raw spans

--- src/inlines/autoLinkRule.ts
+++ src/inlines/autoLinkRule.ts
@@ -1,21 +1,64 @@
-import type Delimiter from "../types/Delimiter";
 import type InlineParserState from "../types/InlineParserState";
 import type InlineRule from "../types/InlineRule";
 import escapeBackslashes from "../utils/escapeBackslashes";
 import isEscaped from "../utils/isEscaped";
 
 const name = "auto_link";
-const precedence = 5;
+const precedence = 10;
 
 function test(state: InlineParserState) {
 	let char = state.src[state.i];
-
 	if (char === "<" && !isEscaped(state.src, state.i)) {
-		return testLinkOpen(state);
-	}
+		let start = state.i;
 
-	if (char === ">" && !isEscaped(state.src, state.i)) {
-		return testLinkClose(state);
+		let closeEnd = -1;
+		for (let j = state.i + 1; j < state.src.length; j++) {
+			if (state.src[j] === ">" && !isEscaped(state.src, j)) {
+				closeEnd = j;
+				break;
+			}
+		}
+		if (closeEnd !== -1) {
+			let content = state.src.substring(state.i + 1, closeEnd);
+
+			// TODO: Should we allow spaces around newlines, so that the user
+			// can split an autolink over multiple lines?
+			if (/\s/.test(content)) {
+				return false;
+			}
+
+			// TODO: Should be stricter in defining what is and isn't an autolink
+			let isEmail = /.@./.test(content);
+			let isLink = /.[\.:]./.test(content);
+
+			if (isEmail || isLink) {
+				let link = encodeURI(decodeURI(escapeBackslashes(content)));
+
+				if (isEmail && !link.startsWith("mailto:") && !link.startsWith("MAILTO:")) {
+					link = `mailto:${link}`;
+				}
+
+				state.delimiters.push({
+					name: "link",
+					markup: char,
+					precedence,
+					line: state.line,
+					start,
+					end: closeEnd,
+					length: 1,
+					canOpen: false,
+					canClose: false,
+					content,
+					info: link,
+				});
+
+				state.i = closeEnd + 1;
+				return true;
+			}
+		}
+
+		state.i += 1;
+		return true;
 	}
 
 	return false;
@@ -25,93 +68,3 @@ export default {
 	name,
 	test,
 } satisfies InlineRule;
-
-function testLinkOpen(state: InlineParserState) {
-	state.delimiters.push({
-		name: "link",
-		markup: "<",
-		precedence,
-		length: 1,
-		line: state.line,
-		start: state.i,
-		end: -1,
-		canOpen: true,
-		canClose: false,
-	});
-	state.i++;
-	return true;
-}
-
-function testLinkClose(state: InlineParserState) {
-	let startDelimiter: Delimiter | undefined;
-	let startDelimiterIndex = -1;
-	let i = state.delimiters.length;
-	while (i--) {
-		let prevDelimiter = state.delimiters[i];
-		if (!prevDelimiter.handled) {
-			if (prevDelimiter.canOpen && prevDelimiter.markup === "<") {
-				startDelimiter = prevDelimiter;
-				startDelimiterIndex = i;
-				break;
-			} else {
-				continue;
-			}
-		}
-	}
-
-	if (startDelimiter) {
-		let content = state.src.substring(startDelimiter.start + startDelimiter.markup.length, state.i);
-
-		if (/\s/.test(content)) {
-			return false;
-		}
-
-		// TODO: Should be stricter in defining what is and isn't an autolink
-		let isEmail = /.@./.test(content);
-		let isLink = /.[\.:]./.test(content);
-
-		if (isEmail || isLink) {
-			// Remove all the opening link delimiters so they won't be picked up
-			// in future. Remove all the opening delimiters between the
-			// delimiters as autolinks can't contain formatting
-			let d = state.delimiters.length;
-			while (d--) {
-				let prevDelimiter = state.delimiters[d];
-				if (prevDelimiter.precedence === precedence) {
-					prevDelimiter.handled = true;
-				} else if (d > startDelimiterIndex) {
-					prevDelimiter.handled = true;
-				}
-			}
-
-			let link = encodeURI(decodeURI(escapeBackslashes(content)));
-
-			if (isEmail && !link.startsWith("mailto:") && !link.startsWith("MAILTO:")) {
-				link = `mailto:${link}`;
-			}
-
-			state.delimiters.push({
-				name,
-				markup: startDelimiter.markup,
-				precedence,
-				length: 1,
-				line: state.line,
-				start: state.i,
-				end: -1,
-				canOpen: false,
-				canClose: true,
-				handled: true,
-				//info: link,
-			});
-
-			startDelimiter.handled = true;
-			startDelimiter.end = state.i;
-			startDelimiter.info = link;
-
-			state.i++;
-			return true;
-		}
-	}
-
-	return false;
-}
