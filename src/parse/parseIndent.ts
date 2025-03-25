a220b2fd182c908a2ed1c6667ff8ca06da9b6079
Andrew K <andrewjk@users.noreply.github.com>
2025-03-25 18:37:30 +1100

    Fix: handle nbsp

--- src/parse/parseIndent.ts
+++ src/parse/parseIndent.ts
@@ -1,6 +1,4 @@
 import type BlockParserState from "../types/BlockParserState";
-import isNewLine from "../utils/isNewLine";
-import isSpace from "../utils/isSpace";
 
 /**
  * Parses indentation, by adding spaces and setting tabs to the next tabstop of
@@ -11,24 +9,32 @@ import isSpace from "../utils/isSpace";
  * stack.
  */
 export default function parseIndent(state: BlockParserState, index: number) {
-	if (isSpace(state.src.charCodeAt(state.i))) {
-		for (; state.i < state.src.length; state.i++) {
-			let char = state.src[state.i];
-			if (char === " ") {
-				// TODO: All the other spaces
+	loop: for (; state.i < state.src.length; state.i++) {
+		switch (state.src.charCodeAt(state.i)) {
+			// space, nbsp
+			case 0x20:
+			case 0xa0: {
 				state.indent += 1;
-			} else if (char === "\t") {
-				// Set spaces to the next tabstop of 4 characters (e.g. for '  \t', set
-				// the spaces to 4)
+				break;
+			}
+			// tab
+			case 0x09: {
+				// Set spaces to the next tabstop of 4 characters (e.g. for
+				// '  \t', set the spaces to 4)
 				state.indent += 4 - (state.indent % 4);
-			} else if (isNewLine(char)) {
+				break;
+			}
+			// cr, lf
+			case 0x0d:
+			case 0x0a: {
 				state.atLineEnd = true;
 				if (state.blankLevel === -1) {
 					state.blankLevel = index;
 				}
-				break;
-			} else {
-				break;
+				break loop;
+			}
+			default: {
+				break loop;
 			}
 		}
 	}
